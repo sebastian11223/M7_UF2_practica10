6319c31f3ca4d9ef90c049a19306c9e44c8ae6b6
sebastian11223 <mcasco@jaumebalmes.net>
2023-02-02 20:40:59 +0100

    hecho 3B

--- Pract10B/Pract10B.py
+++ Pract10B/Pract10B.py
@@ -1,8 +1,34 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-leer = pd.read_csv("List of cities proper by population density11.csv", usecols=["City", "Population"] ).head()
-
+leer = pd.read_csv("List of cities proper by population density11.csv")#, usecols=["City", "Population"] ).head()
 df = pd.DataFrame(leer)
+def poblaciones():
+    df = pd.DataFrame(leer, columns=['City', 'Population'])
+    print(df[:10])
+
+
+def kilometrosCuadrados():
+    km = pd.DataFrame(leer, columns=['City', 'Density KM2'])
+    print(km[:10])
+
+
+
+def MetrosCuadrados():
+    leer['Density  M2'] = leer['Density  M2'].replace(',', '.')
+    M = pd.DataFrame(leer, columns=['City', 'Density  M2'])
+    print(M[:10])
+
+    plt.pie(df["City"])
+    plt.title("Ejercicio 2")
+    return plt.show()
+
+
+    plt.pie(leer.City)
+    #plt.title("Ejercicio 3")
+    plt.show()
+
+MetrosCuadrados()
 
 #def PerciutatTotalPoblacio(ciutat):
     #result = df['City'] == ciutat
@@ -13,7 +39,6 @@ df = pd.DataFrame(leer)
 
 #print(df.loc[df['City'] == 'Mandaluyong'])
 #print(df.loc[df['Population'] == '1,846,513'])
-print(leer)
 #print(leer)
 
 #print(pd.concat([ciudad,popula]))
@@ -21,4 +46,4 @@ print(leer)
 
 #print(leer)
 
-#print(PerciutatTotalPoblacio())
+#print(PerciutatTotalPoblacio()))
---
+++ Pract10B/main.py
@@ -0,0 +1,7 @@
+import Pract10B as P
+#Ecercici 1B
+P.poblaciones()
+#Ecercici 2B
+P.kilometrosCuadrados()
+#Ecercici 3B
+P.MetrosCuadrados()
